72ea13ec36a178f3e288ebd20df38853d0ec21df
stefan-rogin <stefan.rogin@outlook.com>
2025-03-26 00:32:12 +0000

    clean

--- blog-vue-app/database/seeders/DatabaseSeeder.php
+++ blog-vue-app/database/seeders/DatabaseSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use App\Models\Post;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,12 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
         $this->call(PostsTableSeeder::class);
     }
 }
