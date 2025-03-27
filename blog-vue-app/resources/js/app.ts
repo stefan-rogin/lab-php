114482702012ff4a3b5a7fe9ce6a5498398519e4
stefan-rogin <stefan.rogin@outlook.com>
2025-03-27 01:03:59 +0000

    lint

--- blog-vue-app/resources/js/app.ts
+++ blog-vue-app/resources/js/app.ts
@@ -6,16 +6,6 @@ import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
 
-interface ImportMetaEnv {
-    readonly VITE_APP_NAME: string;
-    [key: string]: string | boolean | undefined;
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv;
-    readonly glob: <T>(pattern: string) => Record<string, () => Promise<T>>;
-}
-
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
